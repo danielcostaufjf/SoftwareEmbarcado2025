57a80faa1ee43b4c4013b78860155aed46a79bee
danielcostaufjf <daniel.costa@engenharia.ufjf.br>
2025-10-23 18:08:21 -0300

    fixing some things

--- projeto1/main/main.c
+++ projeto1/main/main.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
-#include <freertos\FreeRTOS.h>
-#include <freertos\task.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <esp_log.h>
 
-void vTask1(void *pvparameters);
-void vTask2(void *pvparameters);
 
-
-void vTask1(void *pvparameters)
+void vTask1(void* pvparameters)
 {
-    ESP_LOGI("Task1", "Task 1 inicializando");
+    ESP_LOGI("T1","Task inicializando...");
+
     while(1)
     {
-        ESP_LOGI("Task1", "Task 1 executando");
+        ESP_LOGI("T1","Task executando...");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void vTask2(void *pvparameters)
+
+void vTask2(void* pvparameters)
 {
-    ESP_LOGI("Task2", "Task 2 inicializando");
+    ESP_LOGW("T2","Task inicializando...");
+
     while(1)
     {
-        ESP_LOGI("Task2", "Task 2 executando");
+        ESP_LOGW("T2","Task executando...");
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
+
 void app_main(void)
 {
-    xTaskCreate(vTask1, "Task 1", 4096, NULL, 1, NULL);
-    xTaskCreate(vTask2, "Task 2", 4096, NULL, 1, NULL);
+    printf("Hello!!!\r\n");
+
+    xTaskCreate(vTask1, "Task 1", 2048, NULL, 2, NULL);
+
+    xTaskCreate(vTask2, "Task 2", 2048, NULL, 2, NULL);
 }
